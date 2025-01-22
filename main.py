2a87308234273cc5b574c7b57d20302a9368cdf1
annashell <annaolegovna3@rambler.ru>
2025-01-22 16:53:30 +0300

    init

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import re
+
+from flask import *
+
+from scripts.database_scripts import init_database, add_syntagma_to_db, add_words_to_db, add_syllables_to_db
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'seg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+def allowed_file(filename: str):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/add_syntagma', methods=['POST'])
+def add_syntagma():
+    if request.method == 'POST':
+        text = request.values['text']
+        if len(text.strip()) > 0:
+            db_name = "synt_syllab.db"
+            if not os.path.exists(db_name):
+                init_database(db_name)
+            synt_index = add_syntagma_to_db(text, db_name)
+            cleaned_string = re.sub(r'[^A-Za-zА-Яа-яЁё\s]', '', text)
+            words = cleaned_string.split()
+            add_words_to_db(words, db_name, synt_index)
+            return render_template("enter_syllables.html", words=words)
+        else:
+            return render_template("error.html", error_text="Текст неверного формата")
+
+
+@app.route('/add_syllables', methods=['POST'])
+def add_syllables():
+    if request.method == 'POST':
+        values = request.values
+        if len(values) > 0:
+            db_name = "synt_syllab.db"
+            if not os.path.exists(db_name):
+                return render_template("error.html", error_text="Не было введено ни одного предложения!")
+            add_syllables_to_db(values, db_name)
+            return render_template("index.html")
+        else:
+            return render_template("error.html", error_text="Слоги не были введены!")
+
+
+@app.route('/find_word', methods=['POST'])
+def find_word():
+    if request.method == 'POST':
+        word = request.values['word']
+        if len(word.strip().split()) == 1:
+            db_name = "synt_syllab.db"
+            if not os.path.exists(db_name):
+                return render_template("error.html", error_text="Не было введено ни одного предложения!")
+            syllables, sentence = find_word_in_db(word.strip(), db_name)
+            return render_template("found_word.html", word=word, sentence=sentence, syllables=syllables)
+        else:
+            return render_template("error.html", error_text="Введите только одно слово!")
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ scripts/database_scripts.py
@@ -0,0 +1,156 @@
+import sqlite3
+
+from flask import render_template
+
+
+def create_init_sql_query():
+    query: str = '''CREATE TABLE syntagma_units (
+                                    syntagma_id INTEGER PRIMARY KEY,
+                                    syntagma_text TEXT NOT NULL);
+
+                                    CREATE TABLE words_units (
+                                        word_id INTEGER PRIMARY KEY,
+                                        word_text TEXT NOT NULL,
+                                        syntagma_index INTEGER,
+                                        FOREIGN KEY(syntagma_index) REFERENCES syntagma_units(syntagma_id));
+
+                                        CREATE TABLE syllable_units (
+                                        syllable_id INTEGER PRIMARY KEY,
+                                        word_index INTEGER NOT NULL,
+                                        pos_in_word INTEGER,
+                                        syllable_text TEXT NOT NULL,
+                                        FOREIGN KEY(word_index) REFERENCES words_units(word_id));'''.strip()
+
+    with open('sqlite_create_tables.sql', "w", encoding="UTF-8") as wh:
+        wh.write(query)
+
+
+def init_database(db_name):
+    try:
+        sqlite_connection = sqlite3.connect(db_name)
+        cursor = sqlite_connection.cursor()
+        print("База данных подключена к SQLite")
+
+        create_init_sql_query()
+
+        with open('sqlite_create_tables.sql', 'r') as sqlite_file:
+            sql_script = sqlite_file.read()
+
+        cursor.executescript(sql_script)
+        sqlite_connection.commit()
+        cursor.close()
+        print("Скрипт SQLite успешно выполнен")
+
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
+        return render_template("error.html", error_text="Ошибка подключения к базе данных")
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
+
+def add_syntagma_to_db(text, db_name):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    insert_query = f"""INSERT INTO syntagma_units (syntagma_id, syntagma_text)  VALUES  (?, ?)"""
+    get_table_size_query = f"""SELECT COUNT(*) FROM syntagma_units"""
+
+    row_count = cursor.execute(get_table_size_query).fetchone()
+
+    if row_count is not None:
+        number_of_syntagmas = row_count[0]
+    else:
+        number_of_syntagmas = 0
+
+    data = [
+        (number_of_syntagmas, text)
+    ]
+
+    cursor.executemany(insert_query, data)
+    connection.commit()
+    cursor.close()
+    return number_of_syntagmas
+
+
+def add_words_to_db(words, db_name, synt_index):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    get_table_size_query = f"""SELECT COUNT(*) FROM words_units"""
+
+    row_count = cursor.execute(get_table_size_query).fetchone()
+
+    if row_count is not None:
+        number_of_words = row_count[0]
+    else:
+        number_of_words = 0
+
+    for i, word in enumerate(words):
+        insert_query = f"""INSERT INTO words_units (word_id, word_text, syntagma_index)  VALUES  (?, ?, ?)"""
+
+        data = [
+            (number_of_words + i, word, synt_index)
+        ]
+
+        cursor.executemany(insert_query, data)
+        connection.commit()
+
+    cursor.close()
+
+
+def add_syllables_to_db(words, db_name):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    get_table_size_query = f"""SELECT COUNT(*) FROM syllable_units"""
+
+    row_count = cursor.execute(get_table_size_query).fetchone()
+
+    if row_count is not None:
+        number_of_syllables = row_count[0]
+    else:
+        number_of_syllables = 0
+
+    syll_count = 0
+
+    for i, word in enumerate(dict(words).values()):
+        word_joined = word.replace("-", "")
+        get_word_index_query = f"""SELECT word_id FROM words_units as wu WHERE wu.word_text='{word_joined}'"""
+        word_count = cursor.execute(get_word_index_query).fetchone()[-1]
+
+        insert_query = f"""INSERT INTO syllable_units (syllable_id, word_index, pos_in_word, syllable_text)  VALUES  (?, ?, ?, ?)"""
+
+        syllables = word.split("-")
+        for j, syll in enumerate(syllables):
+            data = [
+                (number_of_syllables + syll_count, word_count, j, syll)
+            ]
+            syll_count += 1
+            cursor.executemany(insert_query, data)
+            connection.commit()
+
+    cursor.close()
+
+
+def find_word_in_db(word, db_name):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    get_words_query = f"""SELECT * FROM words_units as wu WHERE wu.word_text='{word}'"""
+
+    row_count = cursor.execute(get_table_size_query).fetchone()
+
+    if row_count is not None:
+        number_of_words = row_count[0]
+    else:
+        number_of_words = 0
+
+    for i, word in enumerate(words):
+        insert_query = f"""INSERT INTO words_units (word_id, word_text, syntagma_index)  VALUES  (?, ?, ?)"""
+
+        data = [
+            (number_of_words + i, word, synt_index)
+        ]
+
+        cursor.executemany(insert_query, data)
+        connection.commit()
+
+    cursor.close()
