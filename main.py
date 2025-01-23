1af3faf92991bbd30a4ddc0ed14365496a5387e7
annashell <annaolegovna3@rambler.ru>
2025-01-23 17:23:03 +0300

    fix errors

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ def add_syntagma():
             if not os.path.exists(db_name):
                 init_database(db_name)
             synt_index = add_syntagma_to_db(text, db_name)
-            cleaned_string = re.sub(r'[^A-Za-zА-Яа-яЁё\s]', '', text)
+            cleaned_string = re.sub(r'[^А-Яа-яЁё\s]', '', text)
+            if len(cleaned_string) == 0:
+                return render_template("error.html", error_text="Текст неверного формата")
             words = cleaned_string.split()
             add_words_to_db(words, db_name, synt_index)
             return render_template("enter_syllables.html", words=words)
@@ -43,7 +45,7 @@ def add_syntagma():
 def add_syllables():
     if request.method == 'POST':
         values = request.values
-        if len(values) > 0:
+        if len(values) > 0 and "" not in list(values.values()):
             db_name = "synt_syllab.db"
             if not os.path.exists(db_name):
                 return render_template("error.html", error_text="Не было введено ни одного предложения!")
